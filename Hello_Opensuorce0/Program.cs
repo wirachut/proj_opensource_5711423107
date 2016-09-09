f0c494e9452d9f6bf04006ff019c855ffb43f05f
sirisakbenz <sirisak.sais57@sskru.ac.th>
2016-09-09 14:33:54 +0700

    Add project to repository

---
+++ Hello_Opensuorce0/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_Opensuorce0
+{
+    class Hello
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello My Name is Wirachut Adhivaswut 5711423107");
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();d
+        }
+    }
+}
